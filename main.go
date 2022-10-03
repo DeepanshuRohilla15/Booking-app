299e67803fb62c47ca10e47f7c9f9899e142d154
DeepanshuRohilla15 <20001001027deepanshu@dcrustm.org>
2022-10-03 19:02:09 +0530

    initial

--- main.go
+++ main.go
@@ -6,17 +6,38 @@ func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application\n",conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n",conferenceTickets,remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n",userName,userTickets)
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings,firstName + " " + lastName)
+
+
+	fmt.Printf("Thankyou %v %v for booking %v tickets. You will receive a confirmation email at %v\n",firstName,lastName,userTickets,email)
+	fmt.Printf("%v tickets remaining for %v\n",remainingTickets,conferenceName)
+
+	fmt.Printf("These are all our bookings: %v\n",bookings)
+	
 
 
 	
